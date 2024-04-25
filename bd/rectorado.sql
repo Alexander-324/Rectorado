2e2d66a94edebefcb5ec93691ce7daaf1aa262d8
Alexander Rivas <alexrivasbenitez324@gmail.com>
2024-04-25 18:01:56 -0400

    Actualizacion de la BD

--- bd/rectorado.sql
+++ bd/rectorado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-04-2024 a las 01:39:33
+-- Tiempo de generación: 26-04-2024 a las 00:01:09
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `rectorado`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `areas`
+--
+
+CREATE TABLE `areas` (
+  `id_area` int(11) NOT NULL,
+  `area` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `areas`
+--
+
+INSERT INTO `areas` (`id_area`, `area`) VALUES
+(1, 'FCEA'),
+(4, 'FACET'),
+(5, 'FO'),
+(6, 'MEDICINA'),
+(7, 'FHYCE'),
+(8, 'FCA'),
+(9, 'EXTERNO');
 
 -- --------------------------------------------------------
 
@@ -89,6 +113,19 @@ CREATE TABLE `dependencias` (
   `dependencia` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Volcado de datos para la tabla `dependencias`
+--
+
+INSERT INTO `dependencias` (`id_dependencia`, `dependencia`) VALUES
+(1, 'MESA DE ENTRADA'),
+(2, 'SECRETARIA GENERAL'),
+(3, 'ADMINISTRACION Y FINANZAS'),
+(4, 'CONTROL PREVENTIVO'),
+(5, 'PRESUPUESTO'),
+(6, 'CONTABILIDAD Y PATRIMONIO'),
+(8, 'TESORERIA');
+
 -- --------------------------------------------------------
 
 --
@@ -102,16 +139,19 @@ CREATE TABLE `funcionarios` (
   `apellido` varchar(30) NOT NULL,
   `id_ciudad` int(12) NOT NULL,
   `direccion` varchar(80) NOT NULL,
-  `id_rol` int(11) NOT NULL
+  `id_dependencia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Volcado de datos para la tabla `funcionarios`
 --
 
-INSERT INTO `funcionarios` (`id_funcionario`, `ci`, `nombre`, `apellido`, `id_ciudad`, `direccion`, `id_rol`) VALUES
-(1, '6658363', 'ALEXANDER', 'RIVAS', 15, 'SANTO DOMINGO DE GUZMAN C/ANGEL GIOVINNE', 1),
-(2, '1234567', 'JUAN', 'LOPEZ', 20, 'SAN ANTONIO', 2);
+INSERT INTO `funcionarios` (`id_funcionario`, `ci`, `nombre`, `apellido`, `id_ciudad`, `direccion`, `id_dependencia`) VALUES
+(1, '6658363', 'ALEXANDER', 'RIVAS', 15, 'SANTO DOMINGO DE GUZMAN C/ANGEL GIOVINNE', 4),
+(2, '1234567', 'JUAN', 'LOPEZ', 20, 'SAN ANTONIO', 5),
+(3, '6969100', 'OSCAR', 'CONCHA', 1, 'GUILLERMO ROMANIACH', 3),
+(4, '6761829', 'CRISTHIAN', 'VALENZUELA', 5, 'VILLA ARMANDO', 4),
+(5, '7074066', 'RODNEY', 'ROJAS', 11, 'VILLA ARMANDO', 5);
 
 -- --------------------------------------------------------
 
@@ -129,8 +169,10 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id_rol`, `rol`) VALUES
-(1, 'ADMINISTRADOR'),
-(2, 'SECRETARÍA GENERAL');
+(8, 'RRHH'),
+(10, 'ADMINISTRADOR'),
+(11, 'SECRETARIO'),
+(12, 'VISITANTE');
 
 -- --------------------------------------------------------
 
@@ -140,8 +182,10 @@ INSERT INTO `roles` (`id_rol`, `rol`) VALUES
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `id_funcionario` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
   `correo` varchar(30) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
   `foto` varchar(100) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `password` varchar(80) NOT NULL
@@ -151,12 +195,19 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `id_funcionario`, `correo`, `foto`, `usuario`, `password`) VALUES
-(1, 1, 'alexrivasbenitez324@gmail.com', 'alexander.jpg', 'alexander', 'AleX1.2.');
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `correo`, `id_rol`, `foto`, `usuario`, `password`) VALUES
+(1, 'ALEXANDER', 'RIVAS', 'alexrivasbenitez324@gmail.com', 8, 'alexander.jpg', 'alexander', 'AleX1.2.'),
+(7, 'JUAN', 'PEREZ', 'juan@gmail.com', 11, 'sin_perfil.jpg', 'juan', 'JuaN1.2.');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `areas`
+--
+ALTER TABLE `areas`
+  ADD PRIMARY KEY (`id_area`);
 
 --
 -- Indices de la tabla `ciudades`
@@ -175,7 +226,8 @@ ALTER TABLE `dependencias`
 --
 ALTER TABLE `funcionarios`
   ADD PRIMARY KEY (`id_funcionario`),
-  ADD KEY `id_ciudad` (`id_ciudad`);
+  ADD KEY `id_ciudad` (`id_ciudad`),
+  ADD KEY `funcionarios_ibfk_2` (`id_dependencia`);
 
 --
 -- Indices de la tabla `roles`
@@ -188,11 +240,17 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `id_funcionario` (`id_funcionario`);
+  ADD KEY `id_rol` (`id_rol`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `areas`
+--
+ALTER TABLE `areas`
+  MODIFY `id_area` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `ciudades`
@@ -204,25 +262,25 @@ ALTER TABLE `ciudades`
 -- AUTO_INCREMENT de la tabla `dependencias`
 --
 ALTER TABLE `dependencias`
-  MODIFY `id_dependencia` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_dependencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `funcionarios`
 --
 ALTER TABLE `funcionarios`
-  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
@@ -233,13 +291,13 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `funcionarios`
   ADD CONSTRAINT `funcionarios_ibfk_1` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudades` (`id_ciudad`),
-  ADD CONSTRAINT `funcionarios_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+  ADD CONSTRAINT `funcionarios_ibfk_2` FOREIGN KEY (`id_dependencia`) REFERENCES `dependencias` (`id_dependencia`);
 
 --
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios` (`id_funcionario`);
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
